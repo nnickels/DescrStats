2c943d7a006e1da7802ee0b2e5929034008ee60f
jmausolf <jgm346@gmail.com>
2018-10-03 13:12:45 -0500

    fixing docs

--- DescrStat.py
+++ DescrStat.py
@@ -11,7 +11,7 @@ import numpy as np
 
 
 def DescrStat(datafile):
-    ’’’
+    """
     --------------------------------------------------------------------
     This function prints and returns descriptive statistics on a comma-
     delimited text file of a single variable
@@ -28,13 +28,14 @@ def DescrStat(datafile):
 
     FILES CREATED BY THIS FUNCTION: None
 
-        RETURNS: data_mean, data_std, data_var
-        --------------------------------------------------------------------
-        ’’’
-        data = np.loadtxt(datafile)
-        data_mean = data.mean()
-        data_std = data.std()
-        data_var = data.var()
+    RETURNS: data_mean, data_std, data_var
+    --------------------------------------------------------------------
+    """
+
+    data = np.loadtxt(datafile)
+    data_mean = data.mean()
+    data_std = data.std()
+    data_var = data.var()
 
     print(’Mean of the data is:’, data_mean)
     print(’Standard deviation of the data is:’, data_std)
